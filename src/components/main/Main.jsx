984a843137410624ba3a336d31e7f12a8dd36fb3
Anton Boldyrev <a.m.boldyrev@gmail.com>
2023-04-02 16:45:18 +0500

    initial commit

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,20 @@
+const Main = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Frontend</h2>
+            <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Backend</h2>
+            <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/project/Project.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+import "./styles.css";
+
+const Project = ({ title, img, index }) => {
+  return (
+    <NavLink to={`/project/${index}`}>
+      <li className="project">
+        <img
+          src={img}
+          alt={title}
+          className="project__img"
+        />
+        <h3 className="project__title">{title}</h3>
+      </li>
+    </NavLink>
+  );
+};
+
+export default Project;
